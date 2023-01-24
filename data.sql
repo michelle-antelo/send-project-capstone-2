bbcc980557a72a6ee4fce1fbedc8f355dd89c040
Michelle Antelo <michelle@Michelles-MacBook-Pro.local>
2023-01-24 17:41:03 -0500

    home page structured for CSS

---
+++ data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (email, username, name, user_type, image_url, bio, password)
+    VALUES
+    ("michelle.setter@gmail.com", "michelle.setter", "michelle", "setter", "https://www.linkpicture.com/q/63DE8357-E4B1-4BD6-ADB1-6F34A20C1923.jpeg", "I'm a developer who loves to set!", "michellepass"),
+    ("will.climber@gmail.com", "will.climber", "will", "climber", "https://www.linkpicture.com/q/E9D68C49-04E5-4B61-82A1-056281E822D6.jpeg", "I'm a developer who likes to climebr!", "willpass");
